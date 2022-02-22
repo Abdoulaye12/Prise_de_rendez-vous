5112a7419872a19f99f301a71dab659e492fb11f
Papa Amadou GNING <papagning4@gmail.com>
2022-02-22 21:25:24 +0000

    ListeRDV et Login

---
+++ Iadmin/listeRV.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<body>
+<div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">Rendez-vous</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="dashboard.php" >
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="listeRV.php" class="active">
+          <i class='bx bx-box'></i>
+          <span class="links_name">Liste Rendez-vous</span>
+        </a>
+      </li>
+      <!-- <li>
+          <a href="#">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">Analytics</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-book-alt' ></i>
+            <span class="links_name">Total order</span>
+          </a>
+        </li> -->
+      <li>
+        <a href="#">
+          <i class='bx bx-cog'></i>
+          <span class="links_name">Setting</span>
+        </a>
+      </li>
+      <li class="log_out">
+        <a href="#">
+          <i class='bx bx-log-out'></i>
+          <span class="links_name">Log out</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Liste Rendez-vous</span>
+      </div>
+      <div class="profile-details">
+        <img src="images/profile.jpg" alt="">
+        <span class="admin_name">Amadou</span>
+      </div>
+    </nav>
+    <div class="home-content rv">
+      <div class="sales-boxes rvs">
+        <div class="recent-sales rvsx box">
+          <div class="sales-details">
+            <ul class="details">
+              <li class="topic">Numéro</li>
+            </ul>
+            <ul class="details">
+              <li class="topic">Nom</li>
+            </ul>
+            <ul class="details">
+              <li class="topic">Prénom</li>
+            </ul>
+            <ul class="details">
+              <li class="topic">Service</li>
+            </ul>
+            <ul class="details">
+              <li class="topic">Date RDV</li>
+            </ul>
+            <ul class="details">
+              <li class="topic">Heure RDV</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script>
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".sidebarBtn");
+    sidebarBtn.onclick = function () {
+      sidebar.classList.toggle("active");
+      if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else
+        sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
+  </script>
+</body>
+</html>
